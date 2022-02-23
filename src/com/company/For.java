ed156157b49a067b6488a26429951ba6dc86d33a
Drevenb <nik28tos05@gmail.com>
2022-02-23 18:14:53 +0200

    Add for

---
+++ src/com/company/For.java
@@ -0,0 +1,72 @@
+package com.company;
+
+public class For {
+    //Завдання 1. Сума квадратів
+    /*public static void main(String[] args) {
+        int a;
+        for(int i=2; i<3; i++){
+            a=(i*i+1);
+            System.out.println("Answer is "+a);
+
+        }
+    }
+}
+*/
+
+//Завдання 2. Факторіал
+/*public static void main(String[] args) {
+    int n = 5, fact=1;
+    if (n <= 12) {
+        System.out.println("N!= " + n);
+        for (int i = 0; i < n; i++) {
+            fact *= i + 1;
+            System.out.println(" " + fact);
+        }
+    }
+}
+}
+*/
+//Завдання 3. Ступінь
+/*public static void main(String args[]){
+    int a = 2;
+    int b = 4;
+
+    for (int i = a; i <= b; i++){
+        System.out.printf("Значение " + i + " в квадрате равно %.0f \n", Math.pow(i, 2));
+    }
+}
+}
+*/
+
+    //Завдання 4. 1/0!+1/1!+1/2!+..
+
+  /*  public static void main(String[] args) {
+        int n = 1;
+        int factorial = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            factorial *= i;
+        }
+        double sumOfNambers = 1;
+        for (int i = 1; i <= n; i++)
+        {
+            sumOfNambers += 1.0 / factorial;
+        }
+        System.out.println(" \n"+sumOfNambers);
+    }
+}
+*/
+//Завдання 5. Ступінь
+  public static void main(String[] args) {
+
+          int a = 2;
+          int b = 2;
+
+          for (int i = a; i <= b; i++){
+              System.out.printf("Значение " + i + " в квадрате равно %.0f \n", Math.pow(i, 2));
+          }
+      }
+  }
+
+
+
--- src/com/company/Switch.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Switch {
-    public static void main(String[] args) {
-
-    }
-}
